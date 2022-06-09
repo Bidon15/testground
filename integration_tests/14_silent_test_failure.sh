a4c9dcf15b0e52ec3c420efef340c1473b71e9cf
Laurent Senta <laurent@singulargarden.com>
2022-06-09 16:25:02 +0200

    ci: allow for errors

--- integration_tests/14_silent_test_failure.sh
+++ integration_tests/14_silent_test_failure.sh
@@ -12,17 +12,13 @@ pushd $TEMPDIR
 
 testground healthcheck --runner local:docker --fix
 
+# Note the `&& exit 1` lets us fail the test if the run succeeds.
+# We can't rely on $? when the run fails, because the `err_report` function in header.sh
+# runs first and succeeds (so $? is always 0).
 testground run single --plan=integrations --testcase="issue-1349-silent-failure" \
     --builder=docker:go --runner=local:docker \
-    --instances=1 --wait;
-
-RESULT=$?
+    --instances=1 --wait && exit 1;
 
 echo "terminating remaining containers"
 testground terminate --runner local:docker
 testground terminate --builder docker:go
-
-if [ RESULT = 0 ]; then
-    echo "Test succeeded, but should have failed."
-    exit 1
-fi
