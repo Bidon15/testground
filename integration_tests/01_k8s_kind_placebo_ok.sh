d7ed2e6764fdf262c0595231a7a2716829a040f6
Laurent Senta <laurent@singulargarden.com>
2022-06-10 10:26:32 +0200

    REMOVEME: merge the commits from https://github.com/testground/testground/pull/1353
    Squashed commit of the following:
    
    commit e2847da61b3dc5414f354c17af1434f692258ff1
    Author: Laurent Senta <laurent@singulargarden.com>
    Date:   Thu Jun 9 10:43:38 2022 +0200
    
        fix: converstion error in k8s
    
        ```
        Jun  9 08:39:50.154528  INFO    cleaning up finished pods...
        {"runner": "cluster:k8s", "run_id": "cagr3jtah42asoqjl390"}
        panic: interface conversion: runtime.Object is *v1.Status, not *v1.Event
    
        goroutine 105 [running]:
        github.com/testground/testground/pkg/runner.(*ClusterK8sRunner).watchRunPods.func1(0xc000d89980,
        0xc0003d0700, 0xc0005ff020, 0xc000b864c0)
                /home/circleci/project/pkg/runner/cluster_k8s.go:736 +0x4a9
        created by
        github.com/testground/testground/pkg/runner.(*ClusterK8sRunner).watchRunPods
                /home/circleci/project/pkg/runner/cluster_k8s.go:734 +0x2e7
        ```
    
    commit a53077fdd59c72c2f2b2094366ed7657f14030ea
    Author: Laurent Senta <laurent@singulargarden.com>
    Date:   Wed Jun 8 18:26:08 2022 +0200
    
        ci: tweak the configuration so sync service passes
    
    commit f2ab852347e16b8454003a56501a63778690a3e6
    Author: Laurent Senta <laurent@singulargarden.com>
    Date:   Wed Jun 8 17:34:34 2022 +0200
    
        fix: minimal init the placebo test
    
    commit b6d65c2e1a432bc4ca49ef5694931e937ea68c2f
    Author: Laurent Senta <laurent@singulargarden.com>
    Date:   Wed Jun 8 12:49:05 2022 +0200
    
        fix: placebo examples relies on initialized test

--- integration_tests/01_k8s_kind_placebo_ok.sh
+++ integration_tests/01_k8s_kind_placebo_ok.sh
@@ -15,7 +15,7 @@ docker tag $ARTIFACT testplan:placebo
 kind load docker-image testplan:placebo
 
 pushd $TEMPDIR
-testground healthcheck --runner local:docker --fix
+# testground healthcheck --runner local:docker --fix
 testground run single --runner cluster:k8s --builder docker:go --use-build testplan:placebo --instances 1 --plan placebo --testcase ok --collect --wait | tee run.out
 RUNID=$(awk '/finished run with ID/ { print $9 }' run.out)
 echo "checking run $RUNID"
